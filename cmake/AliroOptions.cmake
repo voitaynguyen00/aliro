502b883d1ddafeba57b252b1f2129e28a05a51fe
voitaynguyen00 <voitaynguyen1234@gmail.com>
2026-06-17 11:13:07 +0700

    build: add CMake helper modules (version, options)
    Change-Id: Ib41c2877e136d0e570630e3bc90a3d10d4206929

--- cmake/AliroOptions.cmake
+++ cmake/AliroOptions.cmake
@@ -1,1 +1,5 @@
-option(ALIRO_BUILD_TESTS "Build test targets" ON)
+option(ALIRO_BUILD_TESTS    "Build test targets"     ON)
+option(ALIRO_BUILD_EXAMPLES "Build example targets"  OFF)
+
+set(ALIRO_CRYPTO_BACKEND "openssl" CACHE STRING "Crypto backend: openssl or mbedtls")
+set_property(CACHE ALIRO_CRYPTO_BACKEND PROPERTY STRINGS openssl mbedtls)
